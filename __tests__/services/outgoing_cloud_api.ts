b0965df9d2b84111be8cc9643a2e65139168ab86
caitano28 <caitano28@gmail.com>
2026-01-10 13:31:09 -0400

    Fix webhook async test with resetModules

--- __tests__/services/outgoing_cloud_api.ts
+++ __tests__/services/outgoing_cloud_api.ts
@@ -3,14 +3,14 @@ jest.mock('../../src/services/blacklist')
 jest.mock('node-fetch')
 import type { Outgoing } from '../../src/services/outgoing'
 import { Store, getStore } from '../../src/services/store'
-import fetch, { Response } from 'node-fetch'
+import type fetchType from 'node-fetch'
 import { DataStore } from '../../src/services/data_store'
 import { MediaStore } from '../../src/services/media_store'
 import { Config, getConfig, defaultConfig, getMessageMetadataDefault, Webhook } from '../../src/services/config'
 import logger from '../../src/services/logger'
 import { isInBlacklistInMemory, addToBlacklistInMemory, isInBlacklist } from '../../src/services/blacklist'
 
-const mockFetch = fetch as jest.MockedFunction<typeof fetch>
+let mockFetch: jest.MockedFunction<typeof fetchType>
 const addToBlacklistMock = addToBlacklistInMemory as jest.MockedFunction<typeof addToBlacklistInMemory>
 const webhook = mock<Webhook>()
 
@@ -33,6 +33,8 @@ describe('service outgoing whatsapp cloud api', () => {
     jest.resetModules()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     ;({ OutgoingCloudApi: OutgoingCloudApiClass } = require('../../src/services/outgoing_cloud_api'))
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    mockFetch = require('node-fetch') as jest.MockedFunction<typeof fetchType>
     config = defaultConfig
     config.ignoreGroupMessages = true
     webhook.timeoutMs = 1
@@ -94,12 +96,11 @@ describe('service outgoing whatsapp cloud api', () => {
     const mockUrl = `${url}/${phone}`
     logger.debug(`Mock url ${mockUrl}`)
     mockFetch.mockReset()
-    expect(fetch).toHaveBeenCalledTimes(0)
-    const response = new Response('ok', { status: 200 })
-    response.ok = true
+    expect(mockFetch).toHaveBeenCalledTimes(0)
+    const response = { ok: true, status: 200, text: async () => 'ok' } as any
     mockFetch.mockResolvedValue(response)
     await service.send(phone!, textPayload)
-    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(mockFetch).toHaveBeenCalledTimes(1)
   })
 
   test('not sendHttp in webhook when is in blacklist', async () => {
