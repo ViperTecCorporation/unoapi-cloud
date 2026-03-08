f693d59eca57cc83fb17a262a1d093fc68bf06f1
caitano28 <caitano28@gmail.com>
2026-03-07 23:25:23 -0400

    test(jest): map legacy baileys imports to mocks

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
+    '^baileys$': '<rootDir>/test-setup/baileys.mock.ts',
+    '^baileys/lib/Utils/logger$': '<rootDir>/test-setup/baileys-logger.mock.ts',
     '^@whiskeysockets/baileys$': '<rootDir>/test-setup/baileys.mock.ts',
     '^@whiskeysockets/baileys/lib/Utils/logger$': '<rootDir>/test-setup/baileys-logger.mock.ts',
   },
